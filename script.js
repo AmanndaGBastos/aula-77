cb1d825716ca1c0eb33edd248d7409e90863a315
AmanndaGBastos <105891173+AmanndaGBastos@users.noreply.github.com>
2022-09-08 19:20:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var first_name = "Ranbir";
+var last_name = "Kaapor";
+
+ var a=10;
+ var b =5;
+// somar numeros
+  var add = a + b;
+   console.log(add);
+
+   //concatenar
+   var nome=first_name.concat(last_name)
+   console.log(nome)
+console.log(first_name.concat(last_name))
+
+ //concat=juntar informações
+
+
+
